99b09fe1b9010a25d6bdcdac8b9725000bfbcff5
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2023-12-21 15:07:43 +0400

    .

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 
 import { authMiddleware, checkAdminAndSuperAdmin } from "../middleware/auth.js";
-import { createGroup, deleteGroup, getGroups, updateGroup } from "../controllers/groupController.js";
+import { createGroup, deleteGroup, getGroups, getGroupsForPagination, updateGroup } from "../controllers/groupController.js";
 import { getCoursesForPagination } from "../controllers/courseController.js";
 
 const router = express.Router();
 
 router.get("/all", getGroups);
-router.get("/pagination", getCoursesForPagination);
+router.get("/pagination", getGroupsForPagination);
 router.post("/", createGroup);
 router.patch("/:id",  updateGroup);
 router.delete("/:id", deleteGroup);
